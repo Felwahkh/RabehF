567cffc87391c817d53cdbbbaa08716986b3e3d9
felwah <felwahruma@gmail.com>
2023-10-14 23:29:29 +0300

    Initial Commit

---
+++ Rabeh/RabehApp.swift
@@ -0,0 +1,17 @@
+//
+//  RabehApp.swift
+//  Rabeh
+//
+//  Created by Felwah on 29/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct RabehApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
